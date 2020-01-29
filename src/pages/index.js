fe7ed2fad99fe9637711fa44df930325304530f2
Michael Parekh <parekh1392@students.d211.org>
2020-01-28 23:09:49 -0600

    fix meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+  <Helmet>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </Helmet>
   <Layout>
     <SEO title="Home" />
   </Layout>
