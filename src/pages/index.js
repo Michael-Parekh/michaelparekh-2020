40c12d2be77b753c5308b4d68dbaa238b84ad38c
Michael Parekh <parekh1392@students.d211.org>
2020-02-02 10:29:18 -0600

    change notation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
   return (
     <Fragment>
       <Helmet>
@@ -19,6 +19,6 @@ const IndexPage = () => (
       </Layout>
     </Fragment>
   )
-)
+}
 
 export default IndexPage
