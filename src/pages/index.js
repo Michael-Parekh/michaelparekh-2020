11edae430115aa4ccc3352709a2b0b45a89b50ad
Michael Parekh <parekh1392@students.d211.org>
2020-02-02 10:21:24 -0600

    remove seo tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,9 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <React.Fragment>
     <Helmet>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Helmet>
-    <Layout>
-      <SEO title="Home" />
-    </Layout>
-  </React.Fragment>
 )
 
 export default IndexPage
