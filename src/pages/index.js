a89708c11612900ee805d4ce1663b2ed68e6dd58
Michael Parekh <parekh1392@students.d211.org>
2020-02-02 10:25:48 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Fragment from "react"
 import { Link } from "gatsby"
 import Helmet from "react-helmet"
 
@@ -7,9 +8,17 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-    <Helmet>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Helmet>
+  return (
+    <Fragment>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      </Helmet>
+
+      <Layout>
+        <SEO title="Home" />
+      </Layout>
+    </Fragment>
+  )
 )
 
 export default IndexPage
